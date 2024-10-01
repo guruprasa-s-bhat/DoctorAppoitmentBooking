8b93b3e1c86384d6579e19350cbba76ee92773bf
Guruprasad Bhat <guruprasadbhat106@gmail.com>
2024-10-02 01:08:40 +0530

    Home page UI

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+
+const About = () => {
+  return <div></div>;
+};
+
+export default About;
